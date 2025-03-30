a4ecb53b03a4f6abe5c721f04a01f4d0d29f20a0
Harris Ibrahimi <98238342+SUP3RBOB@users.noreply.github.com>
2025-03-29 21:00:34 -0400

    Integrated Api (#1)
    Co-authored-by: HenilJariwala <heniljjariwala@gmail.com>

--- src/main/java/com/example/opencloud/OpenCloudRestController.java
+++ src/main/java/com/example/opencloud/OpenCloudRestController.java
@@ -1,22 +1,17 @@
 package com.example.opencloud;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Random;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class OpenCloudRestController {
+
+    @Autowired
+    private WeatherFetcher weatherFetcher;
+
     @GetMapping("/get_weather/{day}")
     public void getWeather(@PathVariable String day) {
-        Random r = new Random();
-        float tempActual = -10 + r.nextFloat() * (30 - (-10));
-        float tempFeelsLike = -10 + r.nextFloat() * (30 - (-10));
-        float wind = -10 + r.nextFloat() * (30 - (-10));
-        float humidity = -10 + r.nextFloat() * (30 - (-10));
-        int uv = r.nextInt() * 10;
-        WeatherCore.getInstance().invoke(new Weather(tempActual, tempFeelsLike, wind, humidity, uv));
-        System.out.println("Got weather for: " + day);
+        weatherFetcher.fetchAndUpdateForecastForDay("Waterloo", day.toLowerCase());
+        System.out.println("Fetched forecast for: " + day);
     }
 }
